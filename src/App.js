15102d1173170d9f7516c50ac49da7f5b55c914c
Mary Mbaire <mbairemary49@gmail.com>
2024-11-29 21:09:53 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (newTask) => setTasks([...tasks, newTask]);
+
+  const updateTask = (updatedTask) => {
+    setTasks(tasks.map(task => (task.id === updatedTask.id ? updatedTask : task)));
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List with Categories</h1>
+      <AddTaskForm onAdd={addTask} />
+      <TaskList tasks={tasks} onUpdate={updateTask} />
+    </div>
+  );
+};
+
+export default App;
 
